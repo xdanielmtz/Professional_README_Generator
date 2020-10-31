39e8f49e5c4d250678688547fef67493c194f91b
Dani JM <xdanielmtz@gmail.com>
2020-10-31 01:07:33 -0400

    wrote and pasted a professional readme onto a function.

--- Develop/index.js
+++ Develop/index.js
@@ -1,3 +1,69 @@
+const fs = require("fs");
+const inquirer = require("inquirer");
+
+
+const mdTemplate = `
+// # { Project Title Here }
+
+
+
+// ## Description
+// { Description }
+
+
+
+// ## Installation of Repo
+// { Instructions here in list format }
+
+
+
+// ## Table of Contents
+// { Table of Contents here }
+
+
+
+// ## How to Use { Project title here }
+// { how to use here }
+
+
+
+// ## Upon closer viewing this application, you will notice: 
+
+
+
+// ## Credits:
+
+
+
+// ## Tests 
+
+
+
+// ## Contributing 
+
+
+
+// ## Questions
+// { Github Profile }
+// { Email here }
+
+
+// ## Deployed Site Link
+// (Safe Music Lovers) https://ayannaaziz.github.io/Project-1/
+
+
+
+// ## Sneak Peak
+// ![Home Page](sml.png)
+
+
+
+// ## License 
+// { License information here }
+`;
+
+fs.writeFile("README.md")
+
 // array of questions for user
 const questions = [
 
